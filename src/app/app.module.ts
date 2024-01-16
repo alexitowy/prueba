b8ed1bcde539ad5bbadad69ad014dbc845be954f
Cyber Geek <snorena22dev@gmail.com>
2024-01-16 19:34:39 +0100

    Actualizacion Home
    Actualizacion visual al home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
@@ -21,5 +21,6 @@ import { SharedModule } from './shared/shared.module';
   exports: [HomeComponent],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  AfterViewInit,
+  OnChanges,
+  AfterViewChecked,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { Movie } from 'src/app/intefaces/movies.interface';
 import { MoviesService } from 'src/app/services/movies.service';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent
+  implements OnInit, AfterViewInit, OnChanges, AfterViewChecked
+{
   trendingMoviesResults: any[] = [];
   topratedMoviesResults: any[] = [];
   actionMovieResults: any[] = [];
   comedyMovieResults: any[] = [];
   sciencefictionMovieResults: any[] = [];
+
+  slider: any;
+  defaultTransform: any;
+  carouselInitialized = false;
 
   secciones: { titulo: string; resultados: Movie[] | undefined }[] = [];
 
@@ -22,7 +42,6 @@ export class HomeComponent implements OnInit {
     private moviesService: MoviesService
   ) {}
 
-  
   ngOnInit(): void {
     // Obtener datos de películas antes de inicializar las secciones
     this.trendingMovies();
@@ -31,7 +50,35 @@ export class HomeComponent implements OnInit {
     this.comedyMovies();
     this.sciencefictionMovies();
   }
-  
+
+  ngAfterViewInit(): void {
+    // Inicializar el carrusel después de cargar los datos
+    of(null)
+      .pipe(delay(0))
+      .subscribe(() => {
+        this.initializeCarousel();
+      });
+  }
+
+  ngAfterViewChecked(): void {
+    // Inicializar el carrusel después de cada verificación de vista
+    this.initializeCarousel();
+  }
+
+  ngOnChanges(): void {
+    // Inicializar el carrusel si hay cambios en los datos
+    this.initializeCarousel();
+  }
+
+  initializeCarousel() {
+    // Obtener el elemento slider después de la inicialización de la vista
+    this.slider = document.getElementById('slider');
+    this.defaultTransform = 0;
+
+    // Marcar el carrusel como inicializado
+    this.carouselInitialized = true;
+  }
+
   initializeSections() {
     this.secciones = [
       { titulo: 'Tendencias', resultados: this.trendingMoviesResults },
@@ -44,7 +91,7 @@ export class HomeComponent implements OnInit {
       },
     ];
   }
-  
+
   trendingMovies() {
     this.moviesService.getTrendingMovies().subscribe((result) => {
       this.trendingMoviesResults = result.results;
@@ -99,5 +146,33 @@ export class HomeComponent implements OnInit {
   handleImageError() {
     console.log('Error al cargar la imagen');
     // Puedes realizar alguna acción adicional si es necesario
+  }
+
+  goNext() {
+    if (this.slider) {
+      this.defaultTransform = this.defaultTransform - 398;
+      if (
+        Math.abs(this.defaultTransform) >=
+        (this.slider.scrollWidth || 0) / 1.7
+      ) {
+        this.defaultTransform = 0;
+      }
+      this.slider.style.transform =
+        'translateX(' + this.defaultTransform + 'px)';
+    }
+    // Utiliza setTimeout para darle tiempo a Angular para detectar los cambios
+    setTimeout(() => {});
+  }
+
+  goPrev() {
+    if (this.slider) {
+      if (Math.abs(this.defaultTransform) === 0) {
+        this.defaultTransform = 0;
+      } else {
+        this.defaultTransform = this.defaultTransform + 398;
+      }
+      this.slider.style.transform =
+        'translateX(' + this.defaultTransform + 'px)';
+    }
   }
 }
